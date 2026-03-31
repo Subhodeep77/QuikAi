319d48869d571b500352989e8510eef9a2be07ae
Subhodeep77 <subhodeepparamanik22413@gmail.com>
2026-04-01 01:08:53 +0530

    frontend+backend

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
 
 const HomePage = () => {
   return (
     <> 
      <Navbar />
+     <Hero />
+     <AiTools />
+     <Testimonial />
     </>
   )
 }
